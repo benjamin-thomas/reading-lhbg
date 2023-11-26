b8bee2dbbab54050815c8f294e84d10ea3043429
Benjamin <benjamin.guy.thomas@gmail.com>
2023-11-26 07:48:52 +0100

    Add F# example

---
+++ fsharp/Program.fs
@@ -0,0 +1,29 @@
+﻿(*
+ * dotnet watch run
+ *)
+type Html = Html of string
+type Structure = Structure of string
+
+let append_ (Structure a) (Structure b) = Structure(a + b)
+let render (Html s) = s
+let el tag content = $"<%s{tag}>%s{content}</%s{tag}>"
+
+let h1_ = Structure << el "h1"
+let p_ = Structure << el "p"
+
+let html_ title (Structure content) =
+    Html(el "html" (el "head" (el "title" title) + el "body" content))
+
+let myHtml =
+    html_
+        "My title"
+        (append_ (h1_ "Heading")
+             (append_
+                  (p_ "Paragraph #1")
+                  (p_ "Paragraph #2")
+             )
+        )
+
+
+let () =
+    printfn $"%s{render myHtml}"
